12fdc7d3235cf03815b21c196b9d47ade3d38731
Haseebm01 <haseebdr01@gmail.com>
2024-05-30 06:41:48 -0700

    modified

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -6,7 +6,6 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import { BsPlay } from 'react-icons/bs';
-
 import { Video } from './../types';
 
 interface IProps {
