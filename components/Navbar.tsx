f43dac1e3e15b61625746089cd40b6f3ce234308
Haseebm01 <haseebdr01@gmail.com>
2024-05-31 17:11:07 -0700

    modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,6 @@ import { GoogleLogin, googleLogout  } from '@react-oauth/google';
 import useAuthStore from '../store/authStore';
 import { IUser } from '../types';
 import { createOrGetUser } from '../utils';
-// import Logo from '../utils/tektak-logo.png';
 import Logo from '../utils/tetak-logo.png';
 
 const Navbar = () => {
