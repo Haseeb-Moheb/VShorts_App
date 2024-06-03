9089810ffaed28c98b6d03434a0029ae1f9fa185
Haseebm01 <haseebdr01@gmail.com>
2024-06-02 21:18:38 -0700

    modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,12 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import { GoogleLogin, googleLogout  } from '@react-oauth/google';
-
 import useAuthStore from '../store/authStore';
 import { IUser } from '../types';
 import { createOrGetUser } from '../utils';
-import Logo from '../utils/tetak-logo.png';
+import Logo from '../utils/tiktik-logo.png'
+// import Logo from '../utils/tektak-logo.png'
+// import Logo from '../utils/talktalk-logo.png'
 
 const Navbar = () => {
   const [user, setUser] = useState<IUser | null>();
@@ -33,7 +34,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
-        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+        <div className='w-[140px] md:w-[129px] md:h-[30px] h-[38px]'>
           <Image
             className='cursor-pointer'
             src={Logo}
